c08dbf2033e52560b43b5ab6a7342b8e1d9d78fb
Vonkensington <vonkensington@gmail.com>
2024-10-21 19:59:03 -0500

    Added navbar and Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 function App() {
-  return (
-    <div class="Header">
+  return (<>
+    <div class="header">
       <h1>Recruiter Helper</h1>
       <p>Find the skills for your job postings</p>
     </div >
+    <div class="navbar">
+      <a href='#'>Link</a>
+      <a href='#'>Link</a>
+      <a href='#'>Link</  a>
+    </div>
+  </>
   );
 }
 
