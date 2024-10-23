953147045a9d952bc3434f97a376a3c880178dfe
Vonkensington <vonkensington@gmail.com>
2024-10-22 20:34:43 -0500

    Abstracting app.js to components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
+import HomeScreen from './Screens/HomeScreen';
 
 function App() {
-  return (<>
-    <div class="header">
-      <h1>Recruiter Helper</h1>
-      <p>Find the skills for your job postings</p>
-    </div >
-    <div class="navbar">
-      <a href='#'>Link</a>
-      <a href='#'>Link</a>
-      <a href='#'>Link</  a>
-    </div>
-  </>
-  );
+  return <HomeScreen />;
 }
 
 export default App;
