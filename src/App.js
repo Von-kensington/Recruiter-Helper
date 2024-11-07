3b741f28636de22b47f66cfd3254d2be243e4a44
Vonkensington <vonkensington@gmail.com>
2024-11-06 20:21:22 -0600

    Fixed structure and added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
-import HomeScreen from './Screens/HomeScreen/HomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Jobs from './pages/Jobs';
+import Profile from './pages/Profile';
+import JobDetails from './pages/JobDetails';
 
 function App() {
-  return <HomeScreen />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/jobs/:id" element={<JobDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
