cae01d774ce0679c6ac9ad22ed2d11e0be754539
Vonkensington <vonkensington@gmail.com>
2024-11-11 15:47:48 -0600

    Navbar Container and Logo Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import "./App.css";
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path='/' element="Login" />
+                <Route path='/' element={<Login />} />
             </Routes>
         </Router>
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Login() {
     return (
-        <div>Login</div>
+        <Container>
+            <Nav>
+                <a href="/">
+                    <img style={{ height: "inherit" }} src="/images/Logo.png"></img>
+                </a>
+                <Login />
+                <Join />
+            </Nav>
+        </Container>
     )
 }
 
+
+const Container = styled.div`
+    padding: 10px;
+    background-color: #FFFFFF;
+    box-shadow: 0px 5px 8px #00000040;
+`
+const Nav = styled.nav`
+    max-width: 1128px;
+    margin: auto;
+    align-items: center;
+    &>a{
+        height: 50px;
+    }
+    display: flex;
+`
 export default Login
